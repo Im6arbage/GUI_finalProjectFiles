87eac27f849c602ea5fcba79a2226919462141a2
jtbrumleyuna <jtbrumley@una.edu>
2014-04-30 14:48:59 -0500

    Almost Finished
    Tweaked the summary page a bit. Added formatting for the floating point
    values, because it was annoying me greatly.

--- ClientPanel.java
+++ ClientPanel.java
@@ -3,9 +3,11 @@ import java.awt.event.*;
 
 import javax.swing.*;
 
+import java.text.DecimalFormat;
 import java.util.*;
 
 import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
 
 
 public class ClientPanel extends JPanel implements ActionListener{
@@ -40,6 +42,8 @@ public class ClientPanel extends JPanel implements ActionListener{
 	private JPanel[] buttonPanel = new JPanel[4];
 	private JPanel totalIncomeButtonPanel;
 	
+	DecimalFormat dec;
+	
 	public static final int BILLS = 0;
 	public static final int FOOD = 1;
 	public static final int GAS = 2;
@@ -58,7 +62,12 @@ public class ClientPanel extends JPanel implements ActionListener{
 		super();
 		super.setLayout(new GridLayout(1, 2));
 		
+		dec = new DecimalFormat("#.##");
+		
 		m_client = cl;
+		
+		TitledBorder summary = BorderFactory.createTitledBorder("Summary");
+		summary.setTitleJustification(TitledBorder.CENTER);
 		
 		totalIncomeButtonPanel = new JPanel();
 		
@@ -68,64 +77,79 @@ public class ClientPanel extends JPanel implements ActionListener{
 		changestotalincome.setForeground(Color.WHITE);
 		
 		totalIncomeButtonPanel.setBackground(Color.GRAY);
-		totalIncomeButtonPanel.setLayout(new BorderLayout());
-		totalIncomeButtonPanel.add(changestotalincome, BorderLayout.WEST);
+		totalIncomeButtonPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		totalIncomeButtonPanel.add(changestotalincome);
+		
 		for(int i = 0; i < buttonPanel.length; ++i)
 		{
 			buttonPanel[i] = new JPanel();
-			buttonPanel[i].setLayout(new BorderLayout());
+			buttonPanel[i].setLayout(new FlowLayout(FlowLayout.LEFT));
 			buttonPanel[i].setBackground(Color.GRAY);
 			modifyExpenses[i] = new JButton("Edit");
 			modifyExpenses[i].setBackground(Color.DARK_GRAY);
 			modifyExpenses[i].setForeground(Color.WHITE);
 			modifyExpenses[i].addActionListener(this);
-			buttonPanel[i].add(modifyExpenses[i], BorderLayout.WEST);
+			buttonPanel[i].add(modifyExpenses[i]);
 		}
 		
 		Expense [] exp;
 		exp = cl.getExpenses();
 		
-		subclientname = new JLabel("Client Name: " + m_client.getName());
+		subclientname = new JLabel(m_client.getName());
 		subclientname.setForeground(Color.WHITE);
 		
-		subtotalincome = new JLabel("Total Income: $" + m_client.getMonthlyIncome());
+		subtotalincome = new JLabel("Total Income: $" + dec.format(m_client.getMonthlyIncome()));
 		subtotalincome.setForeground(Color.WHITE);
-		subgrossincome = new JLabel("Gross Income: $" + m_client.getGrossIncome());
+		subgrossincome = new JLabel("Gross Income: $" + dec.format(m_client.getGrossIncome()));
 		subgrossincome.setForeground(Color.WHITE);		
 		
 		billstext = new JTextField(10);
 		billstext.setText(" ");
-		subbills = new JLabel("Your bills expense is $" + exp[0].getValue() + " of your gross income or " + m_client.getPercentageOf("Bills") + "%");
+		subbills = new JLabel("Your bills expense is $" + dec.format(exp[0].getValue()) + " of your gross income. (" + dec.format(m_client.getPercentageOf("Bills")) + "%)");
 		subbills.setForeground(Color.WHITE);
 		
 		foodtext = new JTextField(10);
 		foodtext.setEditable(false);
-		subfood = new JLabel("Your food expense is $" + exp[1].getValue() + " of your gross income or " + m_client.getPercentageOf("Food") + "%");
+		subfood = new JLabel("Your food expense is $" + dec.format(exp[1].getValue()) + " of your gross income. (" + dec.format(m_client.getPercentageOf("Food")) + "%)");
 		subfood.setForeground(Color.WHITE);
 		
 		gastext = new JTextField(10);
 		gastext.setText("0000");
-		subgas = new JLabel("Your gas expense is $" + exp[2].getValue() + " of your gross income or " + m_client.getPercentageOf("Gas") + "%");
+		subgas = new JLabel("Your gas expense is $" + dec.format(exp[2].getValue()) + " of your gross income. (" + dec.format(m_client.getPercentageOf("Gas")) + "%)");
 		subgas.setForeground(Color.WHITE);
 		
 		enttext = new JTextField(10);
 		enttext.setText("0000");
-		subent = new JLabel("Your entertainment expense is $" + exp[3].getValue() + " of your gross income or " + m_client.getPercentageOf("Entertainment") + "%");
+		subent = new JLabel("Your entertainment expense is $" + dec.format(exp[3].getValue()) + " of your gross income. (" + dec.format(m_client.getPercentageOf("Entertainment")) + "%)");
 		subent.setForeground(Color.WHITE);
 		
-		subgoal = new JLabel("Your target goal savings was " + (m_client.getTargetSavings())*100 + "% of your total income");
+		subgoal = new JLabel("Your target goal savings was " + dec.format((m_client.getTargetSavings())*100) + "% of your total income.");
 		subgoal.setForeground(Color.WHITE);
 		
-		subactualsav = new JLabel("You actually saved " + (100 - m_client.getActualSavingsPercentage()) + "% of your total income");
+		subactualsav = new JLabel("You actually saved " + dec.format((100 - m_client.getActualSavingsPercentage())) + "% of your total income.");
 		subactualsav.setForeground(Color.WHITE);
 		
 		subclientpanel = new JPanel();
-		subclientpanel.setLayout(new GridLayout(0,1));
+		subclientpanel.setLayout(new GridLayout(12, 1));
 		
-		subclientpanel.add(subclientname);
+		JPanel nameformat = new JPanel();
+		JPanel summarypane = new JPanel();
+		
+		nameformat.setLayout(new FlowLayout());
+		nameformat.setBackground(Color.GRAY);
+		subclientname.setFont(new Font("Cambria", Font.ITALIC | Font.BOLD, 20));
+		nameformat.add(subclientname);
+		
+		summarypane.setLayout(new GridLayout(3, 1));
+		summarypane.setBackground(Color.GRAY);
+		summarypane.add(subgrossincome);
+		summarypane.add(subgoal);
+		summarypane.add(subactualsav);
+		summarypane.setBorder(summary);
+		
+		subclientpanel.add(nameformat);
 		subclientpanel.add(subtotalincome);
 		subclientpanel.add(totalIncomeButtonPanel);
-		subclientpanel.add(subgrossincome);
 		subclientpanel.add(subbills);
 		subclientpanel.add(buttonPanel[BILLS]);
 		subclientpanel.add(subfood);
@@ -134,8 +158,7 @@ public class ClientPanel extends JPanel implements ActionListener{
 		subclientpanel.add(buttonPanel[GAS]);
 		subclientpanel.add(subent);
 		subclientpanel.add(buttonPanel[ENTERTAINMENT]);
-		subclientpanel.add(subgoal);
-		subclientpanel.add(subactualsav);
+		subclientpanel.add(summarypane);
 		
 		subclientpanel.setBackground(Color.GRAY);
 		add(subclientpanel);	
@@ -170,15 +193,21 @@ public class ClientPanel extends JPanel implements ActionListener{
 		
 		Expense [] exp;
 		exp = m_client.getExpenses();
-		subclientname.setText("Client Name: " + m_client.getName());
-		subtotalincome.setText("Total Income: $" + m_client.getMonthlyIncome());
-		subgrossincome.setText("Gross Income: $" + m_client.getGrossIncome());
-		subbills.setText("Your bills expense is $" + exp[0].getValue() + " of your gross income or " + m_client.getPercentageOf("Bills") + "%");
-		subfood.setText("Your food expense is $" + exp[1].getValue() + " of your gross income or " + m_client.getPercentageOf("Food") + "%");
-		subgas.setText("Your gas expense is $" + exp[2].getValue() + " of your gross income or " + m_client.getPercentageOf("Gas") + "%");
-		subent.setText("Your entertainment expense is $" + exp[3].getValue() + " of your gross income or " + m_client.getPercentageOf("Entertainment") + "%");
-		subgoal.setText("Your target goal savings was " + (m_client.getTargetSavings())*100 + "% of your total income");
-		subactualsav.setText("You actually saved " + (100 - m_client.getActualSavingsPercentage()) + "% of your total income");
+		subtotalincome.setText("Total Income: $" + dec.format(m_client.getMonthlyIncome()));
+		
+		subgrossincome.setText("Gross Income: $" + dec.format(m_client.getGrossIncome()));
+		
+		subbills.setText("Your bills expense is $" + dec.format(exp[0].getValue()) + " of your gross income. (" + dec.format(m_client.getPercentageOf("Bills")) + "%)");
+		
+		subfood.setText("Your food expense is $" + dec.format(exp[1].getValue()) + " of your gross income. (" + dec.format(m_client.getPercentageOf("Food")) + "%)");
+		
+		subgas.setText("Your gas expense is $" + dec.format(exp[2].getValue()) + " of your gross income. (" + dec.format(m_client.getPercentageOf("Gas")) + "%)");
+		
+		subent.setText("Your entertainment expense is $" + dec.format(exp[3].getValue()) + " of your gross income. (" + dec.format(m_client.getPercentageOf("Entertainment")) + "%)");
+		
+		subgoal.setText("Your target goal savings was " + dec.format((m_client.getTargetSavings())*100) + "% of your total income.");
+		
+		subactualsav.setText("You actually saved " + dec.format((100 - m_client.getActualSavingsPercentage())) + "% of your total income.");
 	}
 	
 	
